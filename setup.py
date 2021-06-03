2385c8b928b7b20c7ce9f496ff37124f90f55394
Pierre Ablin <pierreablin@gmail.com>
2021-06-03 15:48:17 +0200

    fix unit test

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #! /usr/bin/env python
 import os
 import re
-import setuptools  # noqa; we are using a setuptools namespace
-from numpy.distutils.core import setup
+from setuptools import setup
 
 descr = """Kernel Stein Discrepancy descent"""
 
