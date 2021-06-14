b2e3fddff189a0429e70c196a862153673ad7e07
Pierre Ablin <pierreablin@gmail.com>
2021-06-14 16:01:20 +0200

    archive

--- ksddescent/__init__.py
+++ ksddescent/__init__.py
@@ -1,7 +1,7 @@
 # Author: Pierre Ablin <pierre.ablin@ens.fr>
 #
 # License: MIT
-__version__ = '0.1dev'
+__version__ = '0.2'
 
 from .ksd_descent import ksdd_gradient, ksdd_lbfgs  # noqa
 from .contenders import svgd, mmd_lbfgs  # noqa
