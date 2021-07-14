877a0a0a3d1056fc0902e3c1b2f0795b2b8196f2
Pierre Ablin <pierreablin@gmail.com>
2021-07-14 11:40:09 +0200

    change version

--- ksddescent/__init__.py
+++ ksddescent/__init__.py
@@ -1,7 +1,7 @@
 # Author: Pierre Ablin <pierre.ablin@ens.fr>
 #
 # License: MIT
-__version__ = '0.3dev'
+__version__ = '0.3'
 
 from .ksd_descent import ksdd_gradient, ksdd_lbfgs  # noqa
 from .contenders import svgd, mmd_lbfgs  # noqa
